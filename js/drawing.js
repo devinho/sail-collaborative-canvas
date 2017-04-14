f360ba1e8fe5c316eeded1e654db146cbbd0a6d1
Devin <ho.devin05@gmail.com>
2017-04-14 16:29:26 -0500

    starter code filled out

--- js/drawing.js
+++ js/drawing.js
@@ -16,12 +16,14 @@ $(function() {
     return;
   }
 
+  // Part 1 /////////////////////////////////////////////////////////////////////////
+
   //Setup each color palette & add it to the screen
-  // *---
+
   var colors = ["fff","000","f00","0f0","00f","88f","f8d","f88","f05","f80","0f8","cf0","08f","408","ff8","8ff"];
   for (c in colors) {
     var item = $("<div/>").css("background-color", "#" + colors[c]).addClass("colorbox");
-  // --*
+
     item.click((function () {
       var col = colors[c];
       return function () {
@@ -32,13 +34,17 @@ $(function() {
     item.appendTo("#color-holder");
   }
 
+  // Part 1 END ////////////////////////////////////////////////////////////////////////
+
+  // Part 2 /////////////////////////////////////////////////////////////////////////
   //Keep track of if the mouse is up or down
-  // *---
+
   myCanvas.onmousedown = function () {mouseDown = 1;};
   myCanvas.onmouseout = myCanvas.onmouseup = function () {
     mouseDown = 0, lastPoint = null;
   };
-  // --*
+
+  // Part 2 END ////////////////////////////////////////////////////////////////////////
 
   //Draw a line from the mouse's last position to its current position
   var drawLineOnMouseMove = function(e) {
@@ -70,10 +76,10 @@ $(function() {
     lastPoint = [x1, y1];
   }
 
-  // *--
+
+  // Part 3 ////////////////////////////////////////////////////////////////////////
   $(myCanvas).mousemove(drawLineOnMouseMove);
   $(myCanvas).mousedown(drawLineOnMouseMove);
-  // --*
 
   // Add callbacks that are fired any time the pixel data changes and adjusts the canvas appropriately.
   // Note that child_added events will be fired for initial pixel data as well.
@@ -91,14 +97,19 @@ $(function() {
   pixelDataRef.on("child_changed", drawPixel);
   pixelDataRef.on("child_removed", clearPixel);
 
-  // --*
-
-  // CHAT CODE
+  // Part 3 END ////////////////////////////////////////////////////////////////////////
 
   // Get a reference to the root of the chat data.
-// *****
   var messagesRef = new Firebase("https://cssail-224f6.firebaseio.com/chat");
+  // Add a callback that is triggered for each chat message.
+  messagesRef.limitToLast(10).on("child_added", function (snapshot) {
+    var message = snapshot.val();
+    $("<div/>").text(message.text).prepend($("<em/>")
+      .text(message.name + ": ")).appendTo($("#messages-div"));
+    $("#messages-div")[0].scrollTop = $("#messages-div")[0].scrollHeight;
+  });
 
+  // Part 4 ////////////////////////////////////////////////////////////////////////
   function submitChat () {
     var name = $("#name-input").val();
     var text = $("#message-input").val();
@@ -117,11 +128,5 @@ $(function() {
     submitChat();
   });
 
-  // Add a callback that is triggered for each chat message.
-  messagesRef.limitToLast(10).on("child_added", function (snapshot) {
-    var message = snapshot.val();
-    $("<div/>").text(message.text).prepend($("<em/>")
-      .text(message.name + ": ")).appendTo($("#messages-div"));
-    $("#messages-div")[0].scrollTop = $("#messages-div")[0].scrollHeight;
-  });
+  // Part 4 END ////////////////////////////////////////////////////////////////////////
 });
